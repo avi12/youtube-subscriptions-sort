f8aa25560166f3ca8d61515a690e070bd53b5ae5
avi12 <avi6106@gmail.com>
2022-09-23 12:10:24 +0300

    Added content script files

---
+++ contents/content.ts
@@ -0,0 +1,100 @@
+import timeToSeconds from "time-to-seconds";
+import type { PlasmoContentScript } from "plasmo";
+
+let elVideoParents = [];
+const keysPressedInit = {
+  ControlLeft: false,
+  MetaLeft: false,
+  AltLeft: false
+};
+let keysPressed = { ...keysPressedInit };
+const keysPressedNewWindowInit = {
+  ControlLeft: false,
+  MetaLeft: false,
+  AltLeft: false,
+  CapsLock: false
+};
+let keysPressedNewWindow = { ...keysPressedNewWindowInit };
+let isHeldKeys = false;
+
+enum Selectors {
+  videoParent = "ytd-grid-video-renderer, ytd-video-renderer, ytd-rich-item-renderer",
+  videoLink = "a#video-title[href*='/watch'], a#video-title[href*='/shorts'], a#video-title-link[href*='/watch'], a#video-title-link[href*='/shorts']",
+  videoColor = "#video-title",
+  videoTime = "ytd-thumbnail-overlay-time-status-renderer"
+}
+
+function onVideoSelect(e: MouseEvent) {
+  if (!e.ctrlKey || !e.metaKey || !e.altKey) {
+    return;
+  }
+
+  const elParent = (<HTMLElement>e.target).closest(Selectors.videoParent);
+  if (!elParent) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const iVideoParent = elVideoParents.indexOf(elParent);
+  if (iVideoParent === -1) {
+    elVideoParents.push(elParent);
+  } else {
+    elVideoParents.splice(iVideoParent, 1);
+  }
+  elParent.querySelector<HTMLAnchorElement>(Selectors.videoColor).classList.toggle("video-selected");
+  isHeldKeys = true;
+}
+
+function onKeyUp(e: KeyboardEvent) {
+  if (!isHeldKeys) {
+    return;
+  }
+
+  const keysToBePressed = Object.keys(keysPressedInit);
+  keysToBePressed.forEach(key => {
+    keysPressed[key] = keysPressed[key] || e.code === key;
+  });
+
+  const keysToBePressedNewWindow = Object.keys(keysPressedNewWindowInit);
+  keysToBePressedNewWindow.forEach(key => {
+    keysPressedNewWindow[key] = keysPressedNewWindow[key] ||
+      (key === "CapsLock" ? e.getModifierState("CapsLock") : e.code === key);
+  });
+
+  if (!(Object.values(keysPressedNewWindow).every(Boolean) || Object.values(keysPressed).every(Boolean))) {
+    return;
+  }
+
+  const links = elVideoParents
+    .sort((elA, elB) => {
+      const a = elA.querySelector(Selectors.videoTime).textContent;
+      const b = elB.querySelector(Selectors.videoTime).textContent;
+      if (a.includes("SHORTS")) {
+        return -1;
+      }
+      if (b.includes("LIVE")) {
+        return 1;
+      }
+      return timeToSeconds(a) - timeToSeconds(b);
+    })
+    .map(elParent => elParent.querySelector(Selectors.videoLink).href);
+  chrome.runtime.sendMessage({
+    openLinks: [...new Set(links)],
+    isNewWindow: keysPressedNewWindow.CapsLock
+  });
+  elVideoParents.forEach(elParent => {
+    elParent.querySelector(Selectors.videoColor).classList.remove("video-selected");
+  });
+  keysPressed = { ...keysPressedInit };
+  keysPressedNewWindow = { ...keysPressedNewWindowInit };
+  elVideoParents = [];
+  isHeldKeys = false;
+}
+
+document.addEventListener("click", onVideoSelect);
+document.addEventListener("keyup", onKeyUp);
+
+export const config: PlasmoContentScript = {
+  matches: ["https://www.youtube.com/*"]
+};
